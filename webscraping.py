b344628786e18f2ff2b1f746ec78b977e441c157
Bartlomiej Janiszewski <janiszewski.bartlomiej@gmail.com>
2026-02-22 22:30:51 +0100

    step = 4.4

--- webscraping.py
+++ webscraping.py
@@ -1,82 +1,36 @@
-import os.path
 import tkinter
-
-import requests
-import re
-from bs4 import BeautifulSoup
-from tabulate import tabulate
 from pprint import pprint
 from tkinter import Tk, ttk
 
+from tabulate import tabulate
+
 from webscraping2 import extract_and_print_currencies
 
-def repair_title(title: str):
-    return re.sub(r"[| !?><]", "", title)
+pad_x = 12
+font_tuple = "Arial", 14
+headers_items = ["Currency", "BUY (PLN)", "SELL (PLN)", "AVG (PLN)"]
 
-# response = requests.get("https://internetowykantor.pl/kursy-walut")
-response = requests.get("https://www.kantor.pl/kursy-walut")
-response.encoding = "UTF-8"
+table = extract_and_print_currencies()
+pprint(table)
 
-if response.status_code == 200:
-    html_content = response.text
-    # print(html_content)
+print(tabulate(table, headers=headers_items, tablefmt="pretty"))
 
-    page = BeautifulSoup(html_content, "html.parser")
+gui = Tk()
+gui.title("CURRENCIES")
+gui.geometry("500x300")
 
-    if page.title:
-        title = page.title.string.strip()
-        print("Title: ",title)
-    else:
-        title = "No title"
-        print(title)
+headers = tkinter.Label(gui, text="LIVE CURRENCY WEBSCRAPING", font=("Arial", 18))
+headers.pack(pady=15)
 
-    filename = f"pages/{repair_title(title=title)}.html"
+table_frame = ttk.Frame(gui)
+table_frame.pack(pady=25)
 
-    directory = "pages"
+for i, header in enumerate(headers_items):
+    ttk.Label(table_frame, text=headers_items[i], font=font_tuple).grid(row=0, column=i, padx=pad_x)
+    if i != 3:
+        ttk.Label(table_frame, text=table[i][0], font=font_tuple).grid(row=i + 1, column=0, padx=pad_x)
+        ttk.Label(table_frame, text=table[0][i + 1], font=font_tuple).grid(row=1, column=i + 1, padx=pad_x)
+        ttk.Label(table_frame, text=table[1][i + 1], font=font_tuple).grid(row=2, column=i + 1, padx=pad_x)
+        ttk.Label(table_frame, text=table[2][i + 1], font=font_tuple).grid(row=3, column=i + 1, padx=pad_x)
 
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-
-    with open(file=filename, mode="w", encoding="UTF-8") as file:
-        file.write(html_content)
-    print("File saved")
-
-    table = extract_and_print_currencies()
-    pprint(table)
-
-    headers_items = ["Currency", "BUY (PLN)", "SELL (PLN)", "AVG (PLN)"]
-    print(tabulate(table, headers=headers_items, tablefmt="pretty"))
-
-    gui = Tk()
-    gui.title("CURRENCIES")
-    gui.geometry("500x300")
-
-    headers = tkinter.Label(gui, text="LIVE CURRENCY WEBSCRAPING", font=("Arial", 18))
-    headers.pack(pady=15)
-
-    table_frame = ttk.Frame(gui)
-    table_frame.pack(pady=25)
-
-    pad_x = 12
-
-    ttk.Label(table_frame, text=headers_items[0], font=("Arial", 14)).grid(row=0, column=0, padx=pad_x)
-    ttk.Label(table_frame, text=headers_items[1], font=("Arial", 14)).grid(row=0, column=1, padx=pad_x)
-    ttk.Label(table_frame, text=headers_items[2], font=("Arial", 14)).grid(row=0, column=2, padx=pad_x)
-    ttk.Label(table_frame, text=headers_items[3], font=("Arial", 14)).grid(row=0, column=3, padx=pad_x)
-
-    ttk.Label(table_frame, text="USD", font=("Arial", 14)).grid(row=1, column=0, padx=pad_x)
-    ttk.Label(table_frame, text=table[0][0], font=("Arial", 14)).grid(row=1, column=1, padx=pad_x)
-    ttk.Label(table_frame, text=table[0][1], font=("Arial", 14)).grid(row=1, column=2, padx=pad_x)
-    ttk.Label(table_frame, text=table[0][2], font=("Arial", 14)).grid(row=1, column=3, padx=pad_x)
-
-    ttk.Label(table_frame, text="JPY", font=("Arial", 14)).grid(row=2, column=0, padx=pad_x)
-    ttk.Label(table_frame, text=table[1][0], font=("Arial", 14)).grid(row=2, column=1, padx=pad_x)
-    ttk.Label(table_frame, text=table[1][1], font=("Arial", 14)).grid(row=2, column=2, padx=pad_x)
-    ttk.Label(table_frame, text=table[1][2], font=("Arial", 14)).grid(row=2, column=3, padx=pad_x)
-
-    ttk.Label(table_frame, text="EUR", font=("Arial", 14)).grid(row=3, column=0, padx=pad_x)
-    ttk.Label(table_frame, text=table[2][0], font=("Arial", 14)).grid(row=3, column=1, padx=pad_x)
-    ttk.Label(table_frame, text=table[2][1], font=("Arial", 14)).grid(row=3, column=2, padx=pad_x)
-    ttk.Label(table_frame, text=table[2][2], font=("Arial", 14)).grid(row=3, column=3, padx=pad_x)
-
-    gui.mainloop()
+gui.mainloop()
--- webscraping2.py
+++ webscraping2.py
@@ -1,11 +1,17 @@
+import os
+import re
+
+import requests
 from bs4 import BeautifulSoup
 
 table = []
+
 
 def get_content_from_file():
     with open("pages/Aktualnekursywalut-kursNBP-Kantor.pl.html", mode="r", encoding="UTF-8") as file:
         content = file.read()
         return BeautifulSoup(content, "html.parser")
+
 
 def get_price(currency):
     page = get_content_from_file()
@@ -13,12 +19,55 @@ def get_price(currency):
     sell = page.find("span", id=f"sell_{currency}").text.strip()
     return buy, sell
 
+
 def extract_and_print_currencies():
-    currencies = ["USD", "JPY", "EUR" ]
+    currencies = ["USD", "JPY", "EUR"]
+    get_html()
 
     for currency in currencies:
         buy, sell = get_price(currency)
-        avg = (float(buy.replace(",", ".")) + float(sell.replace(",", ".")))/ 2
+        avg = round((float(buy.replace(",", ".")) + float(sell.replace(",", "."))) / 2, 4)
         # print(f"Currency:  {currency},  buy:  {buy} PLN,  sell:  {sell} PLN,  AVG:  {round(avg, 2)} PLN")
         table.append([currency, buy, sell, avg])
     return table
+
+
+def repair_title(title: str):
+    return re.sub(r"[| !?><]", "", title)
+
+
+def get_html():
+    # response = requests.get("https://internetowykantor.pl/kursy-walut")
+    response = requests.get("https://www.kantor.pl/kursy-walut")
+    response.encoding = "UTF-8"
+    if response.status_code == 200:
+        html_content = response.text
+        # print(html_content)
+
+        page = BeautifulSoup(html_content, "html.parser")
+
+        if page.title:
+            title = page.title.string.strip()
+            print("Title: ", title)
+        else:
+            title = "No title"
+            print(title)
+
+        filename = get_html_file_path(title=title)
+
+        directory = "pages"
+
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        with open(file=filename, mode="w", encoding="UTF-8") as file:
+            file.write(html_content)
+            print(f"File saved: {filename}")
+        return filename
+    else:
+        print("CONNECTION PROBLEM")
+        return None
+
+
+def get_html_file_path(title):
+    return f"pages/{repair_title(title=title)}.html"
